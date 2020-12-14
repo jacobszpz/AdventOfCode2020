c9a46ad4d24371820023bc6892945dc0feece4fa
jacobszpz <jacobszpz@protonmail.com>
2020-12-14 23:41:03 +0000

    Day 10 Part Two

--- Day 10: Adapter Array/main.cpp
+++ Day 10: Adapter Array/main.cpp
@@ -3,29 +3,18 @@
 #include <fstream>
 #include <set>
 #include <map>
+#include <vector>
 
 using namespace ::std;
 
-// set to true for part one
-bool countLetter = false;
+// set to false for part one
+bool countPaths = false;
 
-int MrElectric(string text_file) {
-    string line;
-    ifstream input_file(text_file);
-
-    set<int> adapterSet;
+int getDifferences(set<int> adapterSet) {
     int joltage = 0;
 
     map<int, int> diffMap;
     diffMap[3]++;
-
-    if (input_file.is_open()) {
-        while (getline(input_file, line)) {
-            adapterSet.insert(stoi(line));
-        }
-        
-        input_file.close();
-    }
 
     for (auto &adapter : adapterSet) {
         int diff = adapter - joltage;
@@ -37,13 +26,58 @@ int MrElectric(string text_file) {
     return diffMap[1] * diffMap[3];
 }
 
+long long int getPaths(set<int> adapterSet) {
+    const int maxJump = 3;
+    adapterSet.insert(0);
+    vector<int> adapterVector(adapterSet.begin(), adapterSet.end());
+
+    long long int possiblePaths[adapterSet.size()] = {};
+    possiblePaths[0] = 1;
+
+    int i = 0;
+
+    for (auto &adapter : adapterVector) {
+        for (int j = 1; (j <= maxJump && (i + j) < adapterVector.size()); ++j) {
+            if (adapterVector[i + j] <= (adapter + maxJump)) {
+                possiblePaths[i + j] += possiblePaths[i];
+            }
+        }
+
+        i++;
+    }
+
+    return possiblePaths[i - 1];
+}
+
+long long int MrElectric(string text_file) {
+    string line;
+    ifstream input_file(text_file);
+
+    set<int> adapterSet;
+    
+
+    if (input_file.is_open()) {
+        while (getline(input_file, line)) {
+            adapterSet.insert(stoi(line));
+        }
+        
+        input_file.close();
+    }
+
+    if (countPaths) {
+        return getPaths(adapterSet);
+    } else {
+        return getDifferences(adapterSet);
+    }
+}
+
 int main() {
     // Part One
-    //countLetter = true;
+    countPaths = false;
     cout << "Part One Answer: " << MrElectric("input") << endl;
 
     // Part Two
-    /*countLetter = false;
-    cout << "Part Two Answer: " << PassPolicy("input") << endl;*/
+    countPaths = true;
+    cout << "Part Two Answer: " << MrElectric("input") << endl;
     return 0;
 }
